6466414bf57b9d64d7f95be3aafcd7006f1c703c
Bruce <bruce@redravencomputing.com>
2021-04-18 02:26:23 -0600

    Fix some bugs and add reduceers to App

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,6 @@
 export const RECEIVE_TWEETS = 'RECIEVE_TWEETS'
 
-export function receiveTweet (tweets) {
+export function receiveTweets (tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets
