bc29c8583ce26d445ec3ce0a78408a4da3789c75
Bruce <bruce@redravencomputing.com>
2021-04-18 23:06:58 -0600

    Able to reply to tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { formatTweet, formatDate } from '../utils/helpers';
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti';
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -18,7 +19,7 @@ class Tweet extends Component {
   }
   toParent = (e, id) => {
     e.preventDefault();
-    //todo: Redirect to parent Tweet
+    this.props.history.push(`/tweet/${id}`)
   }
   render(){
     const { tweet } = this.props;
@@ -27,11 +28,11 @@ class Tweet extends Component {
     }
 
 const {
-  name, avatar, timestamp, text, hasLiked, likes, replies, parent
+  name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
 } = tweet;
 
     return(
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img
           src={avatar}
           alt={`Avatar of ${name}`}
@@ -59,7 +60,7 @@ const {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -76,4 +77,4 @@ function mapStateToProps({authedUser, users, tweets}, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
